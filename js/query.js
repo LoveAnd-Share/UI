0f5677a9af84f7bd710c8e671f40bf653b01e6b5
match233 <1836167892@qq.com>
2022-05-25 13:01:30 +0800

    表格

---
+++ js/query.js
@@ -0,0 +1,20 @@
+var org;
+        $(document).ready(function(){
+  	    $("#c").click(function(){
+              var xhr = new XMLHttpRequest();
+
+              xhr.open('get','http://localhost:5791/EFStudent/PageStudent?pageIndex=6&pageSize=10',true);
+              xhr.onload = function()
+              {
+                  org = JSON.parse('{"students":'+xhr.responseText+'}');
+                  //alert(org.students[5].id);
+                  for (let index = 0; index < org.students.length; index++) {
+                    $("tbody").append(
+                     "<tr><td>"+org.students[index].id+"</td><td>"+org.students[index].name+"</td><td>"+org.students[index].tename+"</td><td>"+org.students[index].age+"</td></tr>"            
+                    );
+                      
+                  }                                                                                                
+              }
+              xhr.send();
+          });
+ });
