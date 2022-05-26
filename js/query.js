0e350450686e4f2f2982a2a74782ad47a0984555
match233 <1836167892@qq.com>
2022-05-26 16:56:53 +0800

    添加学生信息

--- js/query.js
+++ js/query.js
@@ -1,9 +1,10 @@
 var data;
         $(document).ready(function(){
-  	    $("#c").click(function(){
-              var xhr = new XMLHttpRequest();
+          //
+  	    $("#r").click(function(){
+              let xhr = new XMLHttpRequest();
 
-              xhr.open('get','http://localhost:5791/EFStudent/PageStudent?pageIndex=6&pageSize=10',true);
+              xhr.open('get','http://120.26.194.198:5100/EFStudent/PageStudent?pageIndex=1&pageSize=10',true);
               xhr.onload = function()
               {
                   data = JSON.parse(xhr.responseText);
@@ -16,4 +17,29 @@ var data;
               }
               xhr.send();
           });
+          //添加学生信息
+          $("#btn-add").click(function(){
+              let id = $('#id').val();
+              let name = $('#name').val();
+              let teName = $('#teName').val();
+              let age = $('#age').val();
+              let xhr = new XMLHttpRequest();
+              if(id == '' || name == '' || teName ==  '' || age == '')
+              {
+                alert("添加的信息不完整，请核对");
+              }
+              xhr.open('post','http://localhost:5791/EFStudent/addStu?id='+id+'&name='+name+'&teName='+teName+'&age='+age,true);
+              xhr.onload = function()
+              {
+                if(xhr.status == 200 && xhr.readyState == 4 && xhr.responseText =='success')
+                {
+                  alert('学生信息添加成功');
+                }
+                else
+                {
+                  alert(xhr.responseText);
+                }
+              }
+              xhr.send();
+          })
  });
